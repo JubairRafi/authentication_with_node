49f4eb5923ebee4794fed72e27b6a1577ee504b9
jobairRafi96 <jubairrafi96960@gmail.com>
2020-09-03 00:07:21 -0700

    level 1

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true})
 
+const userSchema = {
+  email: String,
+  password: String
+}
+
+const User = new mongoose.model("User",userSchema)
+
 app.get("/",(req,res)=>{
   res.render("home")
 })
@@ -24,6 +31,43 @@ app.get("/login",(req,res)=>{
 app.get("/register", (req,res)=>{
   res.render("register")
 })
+
+
+app.post("/register",(req,res)=>{
+  const username = req.body.username
+  const password = req.body.password
+  const newUser = new User({
+    email: username,
+    password: password
+  })
+
+  newUser.save(err=>{
+    if(err){
+      console.log(err)
+    }else{
+      res.render("secrets")
+    }
+  })
+
+})
+
+app.post("/login",(req,res)=>{
+  const username = req.body.username
+  const password = req.body.password
+
+  User.findOne({email:username},(err,foundUser)=>{
+    if(err){
+      console.log(err)
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets")
+        }
+      }
+    }
+  })
+})
+
 
 app.listen(3000,err=>{
   console.log("ss")
