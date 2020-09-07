7d71af5133bcbda36dca87fe5aec0f4216ab1a5a
jobairRafi96 <jubairrafi96960@gmail.com>
2020-09-06 18:14:48 -0700

    secret page done

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ mongoose.set("useCreateIndex", true)
 const userSchema =  new mongoose.Schema({
   email: String,
   password: String,
-  googleId:String
+  googleId:String,
+  secret: String
 })
 
 
@@ -99,13 +100,24 @@ app.get("/logout", (req,res)=>{
 })
 
 app.get("/secrets", (req,res)=>{
+  User.find({"secret": {$ne:null}},(err,foundUsers)=>{
+    if (err) {
+      console.log(err)
+    }else{
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets:foundUsers})
+      }
+    }
+  })
+})
+
+app.get("/submit", (req,res)=>{
   if(req.isAuthenticated()){
-    res.render("secrets")
+    res.render("submit")
   }else{
     res.redirect("/login")
   }
 })
-
 
 app.post("/register",(req,res)=>{
   User.register({username:req.body.username}, req.body.password, (err, user)=>{
@@ -133,6 +145,24 @@ app.post("/login",(req,res)=>{
       passport.authenticate("local")(req,res,()=>{
         res.redirect("/secrets")
       })
+    }
+  })
+})
+
+app.post("/submit",(req,res)=>{
+  const submittedSecret = req.body.secret;
+//passport saves user
+
+  User.findById(req.user.id,(err,foundUser)=>{
+    if(err){
+      console.log(err)
+    }else{
+      if(foundUser){
+        foundUser.secret = submittedSecret
+        foundUser.save(()=>{
+          res.redirect("/secrets")
+        })
+      }
     }
   })
 })
